e846b60ea9bb6074f4d2901d3503c185bafd57d2
Stephen Ashby <stephenashby@macmini.lan>
2025-05-30 14:53:51 -0400

    🖼 Update: Use Discogs image for gatefold mockup

--- src/components/AlbumGatefoldCard.jsx
+++ src/components/AlbumGatefoldCard.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import '../styles/gatefold.css';
 
-const AlbumGatefoldCard = () => (
-  <div className="gatefold-container">
-    <div className="gatefold-left">
-      <img src="/images/hotel-california-inside-gatefold-768x512.webp" alt="Album Cover" />
-    </div>
-    <div className="gatefold-right">
-      <h2>Hotel California</h2>
-      <h3>The Eagles</h3>
-      <div>
-        <h4>Side A</h4>
+const AlbumGatefoldCard = () => {
+  return (
+    <div className="gatefold-container">
+      <div className="gatefold-left">
+        <img
+          src="https://i.discogs.com/PZz0RZth-jDXq_vEwv640MTGizMZe5KEc6KTzM6ZKzY/rs:fit/g:sm/q:90/h:600/w:596/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzNTQ4/MTYtMTM4MzkwOTE3/MC0xNzEyLmpwZWc.jpeg"
+          alt="Album Cover"
+          className="album-cover"
+        />
+      </div>
+      <div className="gatefold-right">
+        <h2>Hotel California</h2>
+        <p><strong>Side A:</strong></p>
         <ul>
           <li>Hotel California</li>
           <li>New Kid in Town</li>
           <li>Life in the Fast Lane</li>
           <li>Wasted Time</li>
         </ul>
-        <h4>Side B</h4>
+        <p><strong>Side B:</strong></p>
         <ul>
           <li>Wasted Time (Reprise)</li>
           <li>Victim of Love</li>
@@ -27,7 +30,7 @@ const AlbumGatefoldCard = () => (
         </ul>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AlbumGatefoldCard;
