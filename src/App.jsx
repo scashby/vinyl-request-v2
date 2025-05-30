00eac957d516a5ebcb6796c102da6e916384b8b2
Stephen Ashby <stephenashby@macmini.lan>
2025-05-30 13:32:12 -0400

    🐛 Fix: Restore root route for LandingPage in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import EventsPage from './pages/events/EventsPage';
 import AlbumGatefoldPage from './pages/AlbumGatefoldPage';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/events" element={<EventsPage />} />
         <Route path="/album-demo" element={<AlbumGatefoldPage />} />
       </Routes>
