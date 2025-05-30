c1933e0cbbb53456abaccd1f80868b7eb1783887
Stephen Ashby <stephenashby@macmini.lan>
2025-05-30 12:30:08 -0400

    🎧 UI: refactor BrowseQueue to match event card + Spotify-styled playlist layout

--- src/pages/browse/BrowseQueue.jsx
+++ src/pages/browse/BrowseQueue.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import '../../styles/internal.css';
 import '../../styles/browse-queue.css';
 
-
 const BrowseQueue = () => {
   return (
     <div className="page-wrapper">
@@ -14,50 +13,41 @@ const BrowseQueue = () => {
       </header>
       <main className="page-body browse-queue">
         <aside className="event-sidebar">
-          <img src="/images/event-placeholder.jpg" alt="Event poster" className="event-sidebar-image" />
-          <div className="event-sidebar-details">
-            <h2>Event Title</h2>
-            <p>Date: June 15, 2025</p>
-            <p>Time: 7:00 PM</p>
-            <p>Location: Vinyl Taproom</p>
-          </div>
+          <article className="event-card">
+            <img src="/images/event-header-still.jpg" alt="Vinyl Sunday" className="card-square" />
+            <h2>Vinyl Sunday</h2>
+            <p>June 2, 2025</p>
+          </article>
         </aside>
         <section className="queue-display">
-          <h2>Queue</h2>
-          <table className="queue-table">
-            <thead>
-              <tr>
-                <th>Album</th>
-                <th>Artist</th>
-                <th>Side</th>
-                <th>Votes</th>
-                <th>Like</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>The Dark Side of the Moon</td>
-                <td>Pink Floyd</td>
-                <td>A</td>
-                <td className="votes">★★★★☆</td>
-                <td><button className="vote-button">👍</button></td>
-              </tr>
-              <tr>
-                <td>Abbey Road</td>
-                <td>The Beatles</td>
-                <td>B</td>
-                <td className="votes">★★★★★+</td>
-                <td><button className="vote-button">👍</button></td>
-              </tr>
-              <tr>
-                <td>Random Album</td>
-                <td>Unknown Artist</td>
-                <td>A</td>
-                <td className="votes">☆☆☆☆☆</td>
-                <td><button className="vote-button">👍</button></td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="spotify-header-row">
+            <div>Album</div>
+            <div>Artist</div>
+            <div>Side</div>
+            <div>Votes</div>
+            <div>Like</div>
+          </div>
+          <div className="spotify-row">
+            <div>The Dark Side of the Moon</div>
+            <div>Pink Floyd</div>
+            <div>A</div>
+            <div className="votes">★★★★☆</div>
+            <div><button className="vote-button">👍</button></div>
+          </div>
+          <div className="spotify-row">
+            <div>Abbey Road</div>
+            <div>The Beatles</div>
+            <div>B</div>
+            <div className="votes">★★★★★+</div>
+            <div><button className="vote-button">👍</button></div>
+          </div>
+          <div className="spotify-row">
+            <div>Random Album</div>
+            <div>Unknown Artist</div>
+            <div>A</div>
+            <div className="votes">☆☆☆☆☆</div>
+            <div><button className="vote-button">👍</button></div>
+          </div>
         </section>
       </main>
     </div>
