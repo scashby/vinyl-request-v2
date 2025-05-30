6d5a4883f0e7895176288d4d4129457936887138
Stephen Ashby <stephenashby@macmini.lan>
2025-05-30 13:47:13 -0400

    🧭 Fix: Enable /browse-queue route and remove Supabase dependency from AlbumGatefoldPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import EventsPage from './pages/events/EventsPage';
-import AlbumGatefoldPage from './pages/AlbumGatefoldPage';
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EventsPage from './pages/events/EventsPage';
+import BrowsePage from './pages/browse/BrowsePage';
+import NowPlayingPage from './pages/now-playing/NowPlayingPage';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import EditQueue from './pages/admin/EditQueue';
+import SetNowPlaying from './pages/admin/SetNowPlaying';
+import ImportDiscogs from './pages/admin/ImportDiscogs';
+import BlockSides from './pages/admin/BlockSides';
+import ImportCollection from './pages/admin/ImportCollection';
+import AlbumGatefoldPage from './pages/AlbumGatefoldPage';
+import BrowseQueue from './pages/browse/BrowseQueue';
+
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/events" element={<EventsPage />} />
+        <Route path="/browse" element={<BrowsePage />} />
+        <Route path="/now-playing" element={<NowPlayingPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/edit-queue" element={<EditQueue />} />
+        <Route path="/set-now-playing" element={<SetNowPlaying />} />
+        <Route path="/import-discogs" element={<ImportDiscogs />} />
+        <Route path="/block-sides" element={<BlockSides />} />
+        <Route path="/import-collection" element={<ImportCollection />} />
         <Route path="/album-demo" element={<AlbumGatefoldPage />} />
+        <Route path="/browse-queue" element={<BrowseQueue />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
--- src/pages/AlbumGatefoldPage.jsx
+++ src/pages/AlbumGatefoldPage.jsx
@@ -1,3 +1,5 @@
+
+import React from 'react';
 import PageLayout from '../layouts/PageLayout';
 import AlbumGatefoldCard from '../components/AlbumGatefoldCard';
 
