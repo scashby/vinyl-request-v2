55143d5c715706a5940f0f9974e0112bb9c84387
Stephen Ashby <stephenashby@macmini.lan>
2025-05-30 13:23:37 -0400

    🧪 Add: AlbumGatefold test view page for previewing gatefold layout

---
+++ src/pages/AlbumGatefoldPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AlbumGatefoldCard from '../components/AlbumGatefoldCard';
+
+const AlbumGatefoldPage = () => {
+  return (
+    <div className="page-body">
+      <AlbumGatefoldCard />
+    </div>
+  );
+};
+
+export default AlbumGatefoldPage;
