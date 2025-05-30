65e0f1dee52ca1d17c219fd51ee11eeec0ed6ba5
Stephen Ashby <stephenashby@macmini.lan>
2025-05-30 15:31:32 -0400

    🐛 Fix: Pass mock album prop to AlbumGatefoldCard to prevent destructuring error

--- src/pages/AlbumGatefoldPage.jsx
+++ src/pages/AlbumGatefoldPage.jsx
@@ -3,9 +3,17 @@ import PageLayout from '../layouts/PageLayout';
 import AlbumGatefoldCard from '../components/AlbumGatefoldCard';
 
 const AlbumGatefoldPage = () => {
+  const album = {
+    title: "Hotel California",
+    artist: "Eagles",
+    coverUrl: "https://i.discogs.com/PZz0RZth-jDXq_vEwv640MTGizMZe5KEc6KTzM6ZKzY/rs:fit/g:sm/q:90/h:600/w:596/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzNTQ4/MTYtMTM4MzkwOTE3/MC0xNzEyLmpwZWc.jpeg",
+    sideA: ["Hotel California", "New Kid in Town", "Life in the Fast Lane"],
+    sideB: ["Wasted Time", "Victim of Love", "The Last Resort"]
+  };
+
   return (
     <PageLayout>
-      <AlbumGatefoldCard />
+      <AlbumGatefoldCard album={album} />
     </PageLayout>
   );
 };
