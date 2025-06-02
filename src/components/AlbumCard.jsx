f93753bd11541b05b795c2fbc22eafbcfcbc0560
Stephen Ashby <stephenashby@macmini.lan>
2025-06-01 23:40:52 -0400

    Fix title visibility by renaming .title to .album-title to avoid global CSS override

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -14,7 +14,7 @@ function AlbumCard({ album }) {
         <img src={`/images/${album.image}`} alt={album.title} />
       </Link>
       <div className="info">
-        <p className="title text-blue-600 font-semibold">{album.title}</p>
+        <p className="album-title text-blue-600 font-semibold">{album.title}</p>
         <p className="artist">{album.artist} • {album.year}</p>
       </div>
     </div>
