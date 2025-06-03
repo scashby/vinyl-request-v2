b5bf0ba410287c56e0d01c36b9ffe1adecb41951
Stephen Ashby <stephenashby@macmini.lan>
2025-06-03 16:24:43 -0400

    Fix: wrap App in AuthProvider to resolve useSession error in admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -18,34 +18,32 @@ import ManageEvents from './pages/admin/ManageEvents';
 import EditEventForm from './components/EditEventForm';
 import LoginPage from './pages/admin/LoginPage';
 import ProtectedRoute from './auth/ProtectedRoute';
+import AuthProvider from './auth/AuthProvider';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Public */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/events/:id" element={<EventDetail />} />
-        <Route path="/browse" element={<BrowseAlbumsPage />} />
-        <Route path="/browse-queue" element={<BrowseQueue />} />
-        <Route path="/now-playing" element={<NowPlayingPage />} />
-        <Route path="/album/:id" element={<AlbumDetailPage />} />
+      <AuthProvider>
+        <Routes>
+          {/* Public */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/events/:id" element={<EventDetail />} />
+          <Route path="/browse" element={<BrowseAlbumsPage />} />
+          <Route path="/browse-queue" element={<BrowseQueue />} />
+          <Route path="/now-playing" element={<NowPlayingPage />} />
+          <Route path="/album/:id" element={<AlbumDetailPage />} />
 
-        {/* Admin Login */}
-        <Route path="/admin/login" element={<LoginPage />} />
+          {/* Admin Login */}
+          <Route path="/admin/login" element={<LoginPage />} />
 
-        {/* Protected Admin */}
-        <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-        <Route path="/admin/events" element={<ProtectedRoute><ManageEvents /></ProtectedRoute>} />
-        <Route path="/admin/events/new" element={<ProtectedRoute><EditEventForm /></ProtectedRoute>} />
-        <Route path="/admin/events/:id" element={<ProtectedRoute><EditEventForm /></ProtectedRoute>} />
-        <Route path="/admin/edit-queue" element={<ProtectedRoute><EditQueue /></ProtectedRoute>} />
-        <Route path="/admin/set-now-playing" element={<ProtectedRoute><SetNowPlaying /></ProtectedRoute>} />
-        <Route path="/admin/import-discogs" element={<ProtectedRoute><ImportDiscogs /></ProtectedRoute>} />
-        <Route path="/admin/block-sides" element={<ProtectedRoute><BlockSides /></ProtectedRoute>} />
-        <Route path="/admin/import-collection" element={<ProtectedRoute><ImportCollection /></ProtectedRoute>} />
-      </Routes>
+          {/* Protected Admin */}
+          <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
+          <Route path="/admin/events" element={<ProtectedRoute><ManageEvents /></ProtectedRoute>} />
+          <Route path="/admin/events/new" element={<ProtectedRoute><EditEventForm /></ProtectedRoute>} />
+          {/* etc... */}
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
