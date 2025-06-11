f7bc133f2480e91d430b4bdf6b30bac6d09cab5f
Stephen Ashby <stephenashby@macmini.lan>
2025-06-11 08:06:24 -0400

    Chore: Properly type webpack config in next.config.ts using NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
+// Use the type from Next.js' own type system for Webpack config
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // TypeScript knows config is type 'import("webpack").Configuration'
+    config.resolve = config.resolve || {};
+    config.resolve.extensions = config.resolve.extensions || [];
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+  // ...other Next.js config here
 };
 
 export default nextConfig;
