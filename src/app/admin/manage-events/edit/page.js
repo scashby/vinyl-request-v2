718fe64fc1f4bb7f7b376e018db993f7248e204d
Stephen Ashby <stephenashby@macmini.lan>
2025-06-12 11:31:02 -0400

    fix: add 'use client' and ensure edit page is a valid module

---
+++ src/app/admin/manage-events/edit/page.js
@@ -0,0 +1,8 @@
+// src/app/admin/manage-events/edit/page.js
+
+'use client';
+import EditEventForm from 'components/EditEventForm';
+
+export default function Page() {
+  return <EditEventForm />;
+}
