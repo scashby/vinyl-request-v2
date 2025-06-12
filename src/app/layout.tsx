1b013a9b5ef5542bc243bba4699434fbf630f85f
Stephen Ashby <stephenashby@macmini.lan>
2025-06-12 00:25:58 -0400

    fix: use custom Layout for sitewide navigation and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "styles/globals.css";
 import { Inter } from "next/font/google";
+import Layout from "../components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + " antialiased"}>
-        {children}
+        <Layout>{children}</Layout>
       </body>
     </html>
   );
