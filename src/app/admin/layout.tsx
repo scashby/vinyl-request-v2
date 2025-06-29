54874a034e4526a0b879c9181da422d7a784b2fd
scashby <38795660+scashby@users.noreply.github.com>
2025-06-28 22:17:07 -0400

    update adminlayout to use adminsidebar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,6 +4,7 @@
 import { ReactNode, useEffect } from "react";
 import { AuthProvider, useSession } from "components/AuthProvider";
 import { useRouter } from "next/navigation";
+import AdminSidebar from "components/AdminSidebar";
 
 function RequireAuth({ children }: { children: ReactNode }) {
   const { session } = useSession();
@@ -24,7 +25,12 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <AuthProvider>
       <RequireAuth>
-        <div className="admin-wrapper">{children}</div>
+        <div className="flex">
+          <AdminSidebar />
+          <div className="ml-48 w-full p-4">
+            {children}
+          </div>
+        </div>
       </RequireAuth>
     </AuthProvider>
   );
